3f93bbe59890f02881c80481ff27620879929832
Evgeniy Lushin BFI2001 <lushin_evgeniyyy@rambler.ru>
2022-05-26 01:57:56 +0300

    REST API-service

---
+++ analyzer/__init__.py
@@ -0,0 +1,15 @@
+__author__ = 'Lushin Evgeniy'
+__maintainer__ = __author__
+
+__email__ = 'lushin_evgeniyyy@rambler.ru'
+__license__ = 'MTUCI'
+__version__ = '0.0.1'
+
+
+__all__ = (
+    '__author__',
+    '__email__',
+    '__license__',
+    '__maintainer__',
+    '__version__',
+)
